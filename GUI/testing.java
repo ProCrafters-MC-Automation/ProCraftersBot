03fbb24b96d23f7d068ecdd3abd3ae9e30658e33
1ProCrafters <73192264+1ProCrafters@users.noreply.github.com>
2022-05-06 20:51:21 -0500

    Added GUI

---
+++ GUI/testing.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class testing {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if(input.equals("")){
+            String bot = "node test.js localhost 62405 bot";
+
+            System.out.println(new File("."). getAbsolutePath());
+
+        }
+    }
+}
